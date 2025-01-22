5f7f601986e5ce60a989db023d60ce59b403b05f
muthukumar-29 <muthukumarm.2903@gmail.com>
2025-01-22 15:37:31 +0530

    remove qr reader

--- src/Scanner.jsx
+++ src/Scanner.jsx
@@ -1,10 +1,9 @@
 import { useEffect, useState } from "react"
 import { useNavigate } from "react-router-dom";
-// import {QrReader} from "react-qr-reader";
 
 export default function Scanner() {
 
-    const [scanResult, setScanResult] = useState("");
+    // const [scanResult, setScanResult] = useState("");
 
     const [isLoggedIn, setIsLoggedIn] = useState(localStorage.getItem('isLoggedIn') == "true");
 
@@ -45,31 +44,27 @@ export default function Scanner() {
         }
     }
 
-    const handleScan = (result, error) => {
-        if (result) {
-            setScanResult(result.text);
-        }
+    // const handleScan = (result, error) => {
+    //     if (result) {
+    //         setScanResult(result.text);
+    //     }
 
-        if (error) {
-            console.error("QR Scan Error:", error);
-        }
-    };
+    //     if (error) {
+    //         console.error("QR Scan Error:", error);
+    //     }
+    // };
 
     return (
         <>
             <div>
                 <h1>QR Reader</h1>
 
-                <div className="container">
-                    {/* <QrReader onResult={handleScan} constraints={{ facingMode: "environment" }} style={{ width: "100%" }}></QrReader> */}
-                </div>
-
-                {scanResult && (
+                {/* {scanResult && (
                     <div style={{ marginTop: "20px" }}>
                         <h2>Scanned Result:</h2>
                         <p>{scanResult}</p>
                     </div>
-                )}
+                )} */}
 
                 <button onClick={handleLogout} className="btn btn-danger">
                     Quit Event
