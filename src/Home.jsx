7745d3abd0ac0ca82b53b5e657d4190c8432c60c
muthukumar-29 <muthukumarm.2903@gmail.com>
2025-02-03 11:08:46 +0530

    updated home page

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,20 +1,21 @@
 import Logo from '../public/images/treasure_hunt_logo.jpg'
+import profile from "./assets/profile.png"
 
 export default function Home() {
   return (
     <div>
 
-      <div className='d-flex justify-content-between p-3' style={{ background: '#E8F9FF' }} >
+      <div className='d-flex justify-content-between p-3' style={{ background: '#98D9F9' }} >
         <a href="" className='text-decoration-none text-dark'>Home</a>
         <a href="/login" className='text-decoration-none text-dark'>Login</a>
       </div>
 
-      <div className='text-center'>
-        <h1 className='title bg-transparent' style={{ fontSize: '100px', padding: '200px' }} >MYSTERY HUNT</h1>
+      <div className='text-center initial'>
+        <h1 className='title bg-transparent'>MYSTERY HUNT</h1>
       </div>
-
+      <br />
       <div className='container'>
-        <h2 className='text-center title'>RULES</h2>
+        <h1 className='text-center title'>RULES</h1>
 
         <div className='row'>
           <div className='col-12 col-md-6 col-lg-6'>
@@ -40,6 +41,38 @@ export default function Home() {
       <br />
       <div>
         <h1 className='text-center title'>Organizers</h1>
+        <div className='row'>
+          <div className='col-12 col-lg-2'>
+            <img src={profile} alt="" className='img-fluid p-5' />
+            <p className='font-weight-bold text-center'> Ms. P. BARKAVI <br /> Assistant Professor <br /> Organizing Head </p>
+          </div>
+          <div className='col-12 col-lg-2'>
+            <img src={profile} alt="" className='img-fluid p-5' />
+            <p className='font-weight-bold text-center'> M. MUTHUKUMAR <br /> I - MCA </p>
+          </div>
+          <div className='col-12 col-lg-2'>
+            <img src={profile} alt="" className='img-fluid p-5' />
+            <p className='font-weight-bold text-center'> B. BASKAR <br /> I - MCA </p>
+          </div>
+          <div className='col-12 col-lg-2'>
+            <img src={profile} alt="" className='img-fluid p-5' />
+            <p className='font-weight-bold text-center'> S. BALAJI <br /> I - MCA </p>
+          </div>
+          <div className='col-12 col-lg-2'>
+            <img src={profile} alt="" className='img-fluid p-5' />
+            <p className='font-weight-bold text-center'> B. UDAYAKUMAR <br /> III - BCA 'A'</p>
+          </div>
+          <div className='col-12 col-lg-2'>
+            <img src={profile} alt="" className='img-fluid p-5' />
+            <p className='font-weight-bold text-center'> S. PREM KUMAR <br /> III - BCA 'B'</p>
+          </div>
+        </div>
+      </div>
+
+      <br />
+
+      <div className='text-center'>
+        <p className='text-white copyright p-3 mb-0'>© {new Date().getFullYear()} QMAZE. All Rights Reserved</p>
       </div>
 
 
